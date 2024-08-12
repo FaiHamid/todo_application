36d40847906cdad8c8c6918dcf84a94ee9b0839e
Faiza Hamid <hamid.faiza811@gmail.com>
2024-08-12 23:13:41 +0300

    add todo application

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# React TypeScript Starter Pack
+# Todo application
 
-To use this template click `Use this template`
+- Click the link to see [DEMO LINK](https://FaiHamid.github.io/todo_application/)
 
-### Available Scripts
+## Description
 
-`Deploy` - available to deploy your application to gh-pages
+This is an application, where you can add, remove, update and change completed status of todos. 
+Here I've used React(react-dome, react-transition-group), TypeScript, SCSS, bulma and other technologies.
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+The application fetches and manages todo data from an [API](https://mate-academy.github.io/fe-students-api/) using the fetch API.
+
+### Data Fetching
+The application utilizes the fetch API to retrieve and manage todo data from a remote server. The following operations are supported:
+
+- Fetching Todos: Retrieves the list of todos from the API.
+- Adding Todos: Sends a request to the API to add a new todo.
+- Updating Todos: Sends updates to existing todos to the API.
+- Deleting Todos: Sends a request to the API to remove a todo.
+- Changing Completed Status: Updates the completed status of todos via API requests.
+
+This approach ensures that the todo data is dynamically fetched and synchronized with the backend.
+
+### Dependencies
+* Node v14.16.3 and higher
+* NPM v6.14.4 and higher
+
+### Installing
+* Fork and clone this repository
+* Run `npm install` in your terminal
+* Run `npm start`
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,153 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import React, { useEffect, useState } from 'react';
+import { TodoList } from './components/TodoList';
+import { addTodo, deleteTodo, getTodos, updateTodo } from './api/todos';
+import { ErrorMessages, Filters, LoadingType, Todo } from './types';
+import { TodoHeader } from './components/TodoHeader';
+import { TodoFooter } from './components/TodoFooter';
+import { ErrorNotification } from './components/ErrorNotification';
+import { handleFilteredTodos, makeLoadingObject } from './utils';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filter, setFilter] = useState<Filters>(Filters.All);
+  const [loadingIds, setLoadingIds] = useState<LoadingType>({});
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage(ErrorMessages.LoadError);
+      });
+  }, []);
+
+  const handleAddTodo = (newTodo: Todo): Promise<Todo | void> => {
+    setTempTodo(newTodo);
+
+    return addTodo(newTodo).then(newTodoRes => {
+      setTodos(prevTodos => [...prevTodos, newTodoRes]);
+    });
+  };
+
+  const updateCompletedTodo = (
+    updatedTodo: Todo,
+    key: keyof Todo,
+    value: boolean | string,
+  ) => {
+    return updateTodo({ ...updatedTodo, [key]: value })
+      .then((updatedTodoFromServer: Todo) => {
+        setTodos(prevTodos => {
+          return prevTodos.map(todo =>
+            todo.id === updatedTodo.id ? updatedTodoFromServer : todo,
+          );
+        });
+
+        return false;
+      })
+      .catch(() => {
+        setErrorMessage(ErrorMessages.UpdateError);
+
+        return true;
+      });
+  };
+
+  const handleToggleTodos = () => {
+    const activeTodos = todos.filter(todo => !todo.completed);
+    const completedStatus = activeTodos.length > 0;
+    const todosByStatus = completedStatus ? activeTodos : todos;
+
+    setLoadingIds(makeLoadingObject(todosByStatus));
+
+    Promise.all(
+      todosByStatus.map(todo =>
+        updateTodo({ ...todo, completed: completedStatus }),
+      ),
+    )
+      .then(() =>
+        setTodos(prevTodos => {
+          return prevTodos.map(todo =>
+            todo.completed === completedStatus
+              ? todo
+              : { ...todo, completed: completedStatus },
+          );
+        }),
+      )
+      .catch(() => setErrorMessage(ErrorMessages.UpdateError))
+      .finally(() => setLoadingIds({}));
+  };
+
+  const handleDeleteCompletedTodos = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    setLoadingIds(makeLoadingObject(completedTodos));
+
+    Promise.allSettled(
+      completedTodos.map(todo => deleteTodo(todo.id).then(() => todo)),
+    )
+      .then(values => {
+        values.map(value => {
+          if (value.status === 'rejected') {
+            setErrorMessage(ErrorMessages.DeleteError);
+          } else {
+            setTodos(prevTodos => {
+              const todoID = value.value as Todo;
+
+              return prevTodos.filter(todo => todo.id !== todoID.id);
+            });
+          }
+        });
+      })
+      .finally(() => setLoadingIds({}));
+  };
+
+  const handleDeleteTodo = (todoID: number): Promise<void> => {
+    return deleteTodo(todoID)
+      .then(() => {
+        setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoID));
+      })
+      .catch(() => {
+        setErrorMessage(ErrorMessages.DeleteError);
+      });
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader
+          tempTodo={tempTodo}
+          todos={todos}
+          onToggleTodos={handleToggleTodos}
+          onChangeTempTodo={setTempTodo}
+          onErrorMessage={setErrorMessage}
+          onSubmit={handleAddTodo}
+        />
+
+        <TodoList
+          todos={handleFilteredTodos(todos, filter)}
+          tempTodo={tempTodo}
+          loadingIds={loadingIds}
+          onEdit={updateCompletedTodo}
+          onDelete={handleDeleteTodo}
+        />
+
+        {!!todos.length && (
+          <TodoFooter
+            todos={todos}
+            selectedFilter={filter}
+            onChangeFilter={setFilter}
+            onDeleteCompleted={handleDeleteCompletedTodos}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        onCloseErrorMessage={() => setErrorMessage('')}
+      />
     </div>
   );
 };
